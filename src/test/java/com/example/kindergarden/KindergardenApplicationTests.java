40ad842f049a44bdd91ed22e4b7949c9fc28180b
iskender-dev <88763780+iskender-dev@users.noreply.github.com>
2025-10-10 09:07:32 +0600

    Initializing a spring project

---
+++ src/test/java/com/example/kindergarden/KindergardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kindergarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
